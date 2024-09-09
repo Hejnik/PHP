b89b9680d8dbb507beb1f0d364518772173e38a6
Hejnik <hejnik@gmail.com>
2024-09-09 23:57:46 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="author" content="Krzysztof Hejnicki">
+<meta name="description" content="Strona treningowa">
+<meta name="keywords" content="youtube, szkoła, XXI, wiek, informatyka, GIMP, HTML">
+<title>Strona szkoleniowa</title>
+</head>
+<body>
+
+<h1>Kalkulator Druku 3D</h1>
+<form action="order.php" method="post">
+<label for="zebra">Brelok Zeberka (12.00 PLN/szt.)</label>
+<input type="text" id="zebra" name="zebra"><br><br>
+<label for="zyrafa">Brelok Żyrafka (10.00 PLN/szt.)</label>
+<input type="text" id="zyrafa" name="zyrafa"><br><br>
+<label for="staszic">Brelok Staszic (11.00 PLN/szt.)</label>
+<input type="text" id="staszic" name="staszic"><br><br>
+<label for="baterie">Kontenet na baterie AA/AAA (
+    20.00 PLN/szt.)</label>
+<input type="text" id="baterie" name="baterie"><br><br>
+<input type="submit" value="Wyślij zamówienie"></form>
+</body>
+</html>
