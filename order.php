3f2918c3618c974e5514742602a18f02dd79f941
Szkola 21 Wieku <hejnik@gmail.com>
2024-09-11 21:42:12 +0200

    funkcja

--- order.php
+++ order.php
@@ -1,11 +1,19 @@
 <?php
+
 $zebra = $_POST['zebra']; /* Czas na krótki
 choć bezsensowny komentarz po to abyś to zobaczył */
 $zyrafa = $_POST['zyrafa']; // komentarz jednowierszowy
 $staszic = $_POST['staszic'];
 $kontener = $_POST['baterie']; # komentarz uniksowy
-$cena = $zebra*12+$zyrafa*10+$staszic*11+$kontener*20;
-$cena = number_format($cena, 2, ".", " ");
+
+function podsumowanie($z, $zy, $s, $k){
+    $wynik = $z*12+$zy*10+$s*11+$k*20;
+    $wynik = number_format($wynik, 2, ".", " ");
+    return $wynik;
+
+}
+$cena = podsumowanie($zebra, $zyrafa, $staszic, $kontener);
+
 echo<<<END
 <table align="center" border="1" cellpadding="5" cellspacing="0">
 <tr>
@@ -17,7 +25,7 @@ echo<<<END
 </tr><tr>
 <td>$kontener</td><td>kontenerów na baterie</td>
 </tr><tr>
-<td>Suma</td><td>$cena</td>
+<td>Suma</td><td>$cena zł</td>
 </table>
 <input type="button" onclick="location.href='index.php';" value="Powrót">
 END;
